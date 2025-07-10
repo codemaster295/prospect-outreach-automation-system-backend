aa011d3ed014e3c60429428d0e42fe4e63613812
Manasvi20481 <vam.codesprintconsulting@gmail.com>
2025-07-10 09:47:09 +0530

    fix https issue in nodemailer file

--- src/clients/NodeMailer.ts
+++ src/clients/NodeMailer.ts
@@ -2,12 +2,27 @@ import nodemailer from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 class NodeMailerClient {
-    constructor(private readonly config: SMTPTransport.Options) {
-        this.config = config;
+  private transporter;
+
+  constructor(private readonly config: SMTPTransport.Options) {
+
+    if (this.config.port === 465) {
+      this.config.secure = true;
+    } else if (this.config.port === 587) {
+      this.config.secure = false; 
+      this.config.requireTLS = true;
     }
-    getEmailClient() {
-        return nodemailer.createTransport(this.config);
-    }
+
+    this.config.tls = {
+      rejectUnauthorized: true, 
+    };
+
+    this.transporter = nodemailer.createTransport(this.config);
+  }
+
+  getEmailClient() {
+    return this.transporter;
+  }
 }
 
 export default NodeMailerClient;
